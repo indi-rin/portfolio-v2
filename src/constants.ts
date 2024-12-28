b9acdabf002f7d96abc63cb58dd94af4ff65825c
Indi Rinearson <indi.rinearson@gmail.com>
2024-12-28 12:02:49 -0800

    add colors to tailwind config

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export enum COLORS {
+  black = "#02010A",
+  federalBlue = "#140152",
+  vistaBlue = "#8F95D3",
+  thistle = "#D3C4E3",
+  orchidPink = "#DBB1BC",
+  snow = "#F9F1F3",
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,21 @@
 import type { Config } from "tailwindcss";
+import { COLORS } from "./src/constants";
 
 export default {
   content: [
-<<<<<<< HEAD
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-=======
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
->>>>>>> bd8c3bb (Initial commit from Create Next App)
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: "#02010A",
+        federalBlue: "#140152",
+        vistaBlue: "#8F95D3",
+        thistle: "#D3C4E3",
+        orchidPink: "#DBB1BC",
+        snow: "#F9F1F3",
       },
     },
   },
